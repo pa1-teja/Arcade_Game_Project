fc121aa8c2bdf0907742104665b20142711f11f8
KONA VENKATA SAI PAVAN TEJA <pa1-teja@users.noreply.github.com>
2017-07-13 22:59:48 +0530

    Final commit again.

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ FEND-Arcade-game-Project
 # How to run the game
 
 Download the repository in your desktop, unzip it and open the "index.html" file in your browser to play the game.
+
+# How to play the Game
+  - Use Left,Right,Up and Down arrow keys to move the player
+  - Player should reach to the water level to go to win the game.
+  - If the player reaches the water body, The Game Won number will be incremented by 1.
+  - If the player collides with the enemy, Player position will reset to the initial position at the starting position (i.e. in the grass).
+  - If the player collides with the enemy, The Game Lost number will be incremented by 1.
--- js/app.js
+++ js/app.js
@@ -1,32 +1,39 @@
+'use strict';
 var noOfGamesWon = 0;
 var noOfGamesLost = 0;
 
+// Enemies setup
+var Enemy = function(x, y) {
+   // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
 
-var Enemy = function(x, y, speed) {
-  
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
-    this.speed = speed;
+    this.speed = Math.random()*150 + 100;
 };
 
-
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-   
+     // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.   
     this.x = this.x + (this.speed * dt);
     if (this.x > 400) {
         this.x = 0;
     }
-    return this.x;
 };
 
-
+// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 
-
+// Now write your own player class
 var Player = function(x, y) {
   
     this.sprite = 'images/char-boy.png';
@@ -34,36 +41,18 @@ var Player = function(x, y) {
     this.y = y;
 };
 
-
+// This class requires an update(), render().
 Player.prototype.update = function() {
    
     this.collisionOccurred();
     this.distanceCheck();
 };
 
-Player.prototype.playerBoundries = function() {
-    
-    if (this.x < 0 || this.x > 400) {
-        if (this.x < 0) {
-            this.x = 0;
-        } else {
-            this.x = 400;
-        }
-    }
-    
-    if (this.y < 0 || this.y > 520) {
-        if (this.y < 0) {
-            this.y = 0;
-        } else {
-            this.y = 520;
-        }
-    }
-};
-
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// a handleInput() method.
 Player.prototype.handleInput = function(direction) {
 
     switch (direction) {
@@ -82,39 +71,65 @@ Player.prototype.handleInput = function(direction) {
     }
 };
 
-Player.prototype.Restart = function() {
+
+// mention the boundries for the player to be within.
+Player.prototype.playerBoundries = function() {
+    
+    if (this.x < 0 || this.x > 400) {
+        if (this.x < 0) {
+            this.x = 0;
+        } else {
+            this.x = 400;
+        }
+    }
+    
+    if (this.y < 0 || this.y > 320) {
+        if (this.y < 0) {
+            this.y = 0;
+        } else {
+            this.y = 320;
+        }
+    }
+};
+
+
+// bring back the player to the starting position.
+Player.prototype.restart = function() {
     this.x = 200;
     this.y = 320;
 };
 
-
+// check for the collision between the player and the bug.
 Player.prototype.collisionOccurred = function() {
     for (var i = 0; i < allBugs.length; i++) {
         if (this.x < allBugs[i].x + 75 && this.x + 65 > allBugs[i].x && this.y < allBugs[i].y + 50 && 70 + this.y > allBugs[i].y) {
             noOfGamesLost++;
             console.log("Games Lost : " + noOfGamesLost);
             this.scoreUpdate();
-            this.Restart(); 
+            this.restart(); 
         }
     }
 };
 
+// check if the player reached the water and act accordingly.
 Player.prototype.distanceCheck = function() {
     if (this.y === 0) { 
          noOfGamesWon++;
          console.log("games_won : " + noOfGamesWon);
          this.scoreUpdate();
-        this.Restart(); 
+        this.restart(); 
     }
 };
 
-
+// update the score on the screen
 Player.prototype.scoreUpdate = function() {
      document.getElementsByClassName('games_won')[0].innerHTML = 'Games Won: ' + noOfGamesWon;
      document.getElementsByClassName('games_lost')[0].innerHTML = 'Games Lost: ' + noOfGamesLost;
 };
 
-
+// Now instantiate your objects.
+// Place all enemy objects in an array called allBugs
+// Place the player object in a variable called player
 var allBugs = [];
 var e1 = new Enemy(10, 70, 300);
 var e2 = new Enemy(10, 150, 170);
@@ -125,6 +140,8 @@ var allBugs = [e1, e2, e3];
 
 var player = new Player(200, 320);
 
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
